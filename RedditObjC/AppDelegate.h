e5d647227a1288267b03ce2b3f7f7847da85e157
Cleo "Lo" Howard <lolearningcode@gmail.com>
2019-05-21 23:55:52 -0600

    Initial Commit

---
+++ RedditObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditObjC
+//
+//  Created by Lo Howard on 5/21/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedditObjC
+//
+//  Created by Lo Howard on 5/21/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
